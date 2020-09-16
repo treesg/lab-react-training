af8bbbde8d514254aa1cd618b1254dd2d997d17d
Sebastian Grana <sebastiangrana1@gmail.com>
2020-09-16 16:43:40 -0400

    iteration 13

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import profiles from './data/berlin.json';
 import visa from './visa.png';
 import mastercard from './mastercard.png';
 //import './App.css';
@@ -93,6 +94,34 @@ function App() {
 
         <h2>LikeButton</h2>
         <LikeButton /> <LikeButton />
+
+        <h2>ClickablePicture</h2>
+        <ClickablePicture
+          img='/img/persons/maxence.png'
+          imgClicked='/img/persons/maxence-glasses.png'
+        />
+
+        <h2>Dice</h2>
+        <Dice />
+
+        <h2>Carousel</h2>
+        <Carousel
+          imgs={[
+            'https://randomuser.me/api/portraits/women/1.jpg',
+            'https://randomuser.me/api/portraits/men/1.jpg',
+            'https://randomuser.me/api/portraits/women/2.jpg',
+            'https://randomuser.me/api/portraits/men/2.jpg'
+          ]}
+        />
+
+        <h2>NumbersTable</h2>
+        <NumbersTable limit={12} />
+
+        <h2>Facebook</h2>
+        <Facebook />
+
+        <h2>SignupPage</h2>
+        <SignupPage />
     </div>
   );
 }
@@ -109,7 +138,7 @@ function IdCard(props) {
       <li><b>Birth: </b><span>{props.birth.toDateString()}</span></li>
     </ul>
   </div>
-  )
+  );
 }
 
 function Greetings(props) {
@@ -121,7 +150,7 @@ function Greetings(props) {
   }
   return (
     <h3 style={{border: "1px solid black", textAlign: 'left', padding: '5px', margin: '5px'}}>{greetings[props.lang]} {props.children}</h3>
-  )
+  );
 }
 
 function Random(props) {
@@ -130,7 +159,7 @@ function Random(props) {
     <h3 style={{border: "1px solid black", textAlign: 'left', padding: '5px', margin: '5px'}}>
       Random value between {props.min} and {props.max} ={'>'} {num}
     </h3>
-  )
+  );
 }
 
 function BoxColor(props) {
@@ -144,11 +173,16 @@ function BoxColor(props) {
     return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
   }
   return (
-    <h3 style={{textAlign: 'center', border: "1px solid black", padding: '20px', margin: '5px', backgroundColor: `rgb(${props.r},${props.g},${props.b})`}}>
+    <h3 style={{
+      textAlign: 'center',
+      border: "1px solid black",
+      padding: '20px', margin: '5px',
+      backgroundColor: `rgb(${props.r},${props.g},${props.b})`
+    }}>
       rgb({props.r},{props.g},{props.b}) <br></br>
       {rgbToHex(props.r, props.g, props.b)}
     </h3>
-  )
+  );
 }
 
 function CreditCard(props) {
@@ -159,16 +193,22 @@ function CreditCard(props) {
   let number = props.number.toString()
   let year = props.expirationYear.toString()
   return (
-    <div style={{width: '300px', backgroundColor: props.bgColor, color: 'white', padding: '10px', margin: '20px', display: 'inline-block', borderRadius: '10px'}}>
+    <div style={{width: '300px',
+      backgroundColor: props.bgColor,
+      color: props.color, padding: '10px',
+      margin: '20px',
+      display: 'inline-block',
+      borderRadius: '10px'
+    }}>
       <div style = {{textAlign: 'right'}}><img src={cards[props.type]} alt='Card Provider' style={{maxWidth: '100%', height: '20px'}}></img></div>
-      <div style={{textAlign: 'center', fontSize: '1.9em', margin: '20px 0', mixBlendMode: 'difference'}}>•••• •••• •••• {number.substring(number.length - 4)}</div>
-      <div style={{mixBlendMode: 'difference'}}>
+      <div style={{textAlign: 'center', fontSize: '1.9em', margin: '20px 0'}}>•••• •••• •••• {number.substring(number.length - 4)}</div>
+      <div style={{}}>
         <span>Expires: {props.expirationMonth}/{year.substring(2)}</span>
         <span style={{marginLeft: '20px'}}>{props.bank}</span>
       </div>
-      <div style={{mixBlendMode: 'difference'}}>{props.owner}</div>
+      <div style={{}}>{props.owner}</div>
     </div>
-  )
+  );
 }
 
 function Rating(props) {
@@ -184,11 +224,10 @@ function Rating(props) {
     <div style={{fontSize: '2rem'}}>
       {result}
     </div>
-  )
+  );
 }
 
 function DriverCard(props) {
-  
   return (
     <div style={{display: 'flex', justifyContent: 'center', textAlign: 'center', padding: '15px', margin: '10px', borderRadius: '10px', backgroundColor: '#425cbb'}}>
       <img src={props.img} height='100px' width='100px' style={{objectFit: 'cover', borderRadius: '50%'}} alt="Driver"></img>
@@ -198,33 +237,174 @@ function DriverCard(props) {
         <span>{props.car.model} - {props.car.licensePlate}</span>
       </div>
     </div>
-  )
+  );
 }
 
-class LikeButton extends React.Component {
-  constructor(props){
-    super(props);
-    this.click = this.click.bind(this);
-    this.state = {
-      likes: 0
-    };
-  }
+function LikeButton() {
   
-  click(e) {
-    this.setState((oldState, props) => {
-      return {
-        likes: oldState.likes + 1
-      }
-    });
+  const [likes, setLikes] = useState(0);
+
+  let colors = ['purple','blue','green','yellow','orange','red'];
+  const [color, setColor] = useState(0);
+
+  function colorIndex(num) {
+    if (num + 1 > 5) return 0;
+    return num + 1;
   }
 
-  render(){
   return (
-    <button onClick={this.click}>
-      {this.state.likes} Likes
+    <button onClick={() => {
+      setLikes(likes + 1);
+      setColor(colorIndex(color));
+      }} 
+    style={{border: '1px solid grey',
+    borderRadius: 0,
+    backgroundColor: `${colors[color]}`,
+    color: 'white'}}>
+      {likes} Likes
     </button>
-  )
+  );
+}
+
+function ClickablePicture(props) {
+  const imgNoGlasses = 'img/persons/maxence.png',
+  imgGlasses = 'img/persons/maxence-glasses.png';
+  const [img, setImg] = useState(imgNoGlasses);
+
+  function imgSwap() {
+    if (img === imgNoGlasses) {
+      setImg(imgGlasses)
+    } else {
+      setImg(imgNoGlasses)
+    }
   }
+
+  return (
+    <img onClick={imgSwap}
+    src={img}
+    alt='man that equips/dequips glasses when clicked'
+    style={{cursor: 'pointer', height: '100px'}}></img>
+  );
+}
+
+function Dice() {
+
+  const [dice, setDice] = useState(rand());
+
+  function rand() {
+    return Math.floor((Math.random() * 6) + 1);
+  }
+
+  let diceArr = {
+    'empty': 'img/dice-empty.png',
+    '1': 'img/dice1.png',
+    '2': 'img/dice2.png',
+    '3': 'img/dice3.png',
+    '4': 'img/dice4.png',
+    '5': 'img/dice5.png',
+    '6': 'img/dice6.png',
+  };
+
+  return (
+    <img src={diceArr[dice]}
+    alt="dice"
+    onClick={() => {
+      setDice('empty')
+      setTimeout(() => setDice(rand()), 1000)
+    }}
+    width='100px'
+    ></img>
+  );
+}
+
+function Carousel(props) {
+
+  const [img, setImg] = useState(0);
+
+  function indexRight() {
+    if (props.imgs[img+1] === undefined) {
+      setImg(0);
+    } else {
+      setImg(img + 1);
+    }
+  }
+
+  function indexLeft() {
+    if (props.imgs[img-1] === undefined) {
+      setImg(props.imgs.length-1);
+    } else {
+      setImg(img - 1);
+    }
+  }
+  
+  return(
+    <div>
+      <button onClick={indexLeft}>Left</button>
+      <img src={props.imgs[img]}
+      alt="person"></img>
+      <button onClick={indexRight}>Right</button>
+    </div>
+  );
+}
+
+function NumbersTable(props) {
+  let result = [];
+  for (let i = 1; i <= props.limit; i++){
+    let color;
+    if (i%2 !== 0){
+      color = 'white';
+    } else {
+      color = 'red';
+    }
+    result.push(
+      <div
+      style={{
+        height: '50px',
+        width: '50px',
+        textAlign: 'center',
+        lineHeight: '50px',
+        display: 'inline-block',
+        backgroundColor: color,
+        border: '1px solid black'
+      }}
+      key={'numberTable'+i}
+      >{i}</div>
+    );
+  }
+  return (
+    <div>
+     {result} 
+    </div>
+  );
+}
+
+function Facebook() {
+  return (
+  <div>
+  {profiles.map(profile => (
+        <div style={{border: "1px solid black", display: "flex", padding: 5 + "px", margin: 5 + "px"}} key={profile.firstName+profile.lastName}>
+          <img src={profile.img} style={{objectFit: 'cover', width: '128px', height: "128px"}} alt="man"></img>
+          <ul style={{listStyleType: "none", padding: 0, margin: 0, marginLeft: 5 + "px", textAlign: "left"}}>
+            <li><b>First Name: </b><span>{profile.firstName}</span></li>
+            <li><b>Last Name: </b><span>{profile.lastName}</span></li>
+            <li><b>Country: </b><span>{profile.country}</span></li>
+            <li><b>Type: </b><span>{profile.isStudent ? "Student" : "Teacher"}</span></li>
+          </ul>
+        </div>
+    ))}
+  </div>
+  );
+}
+
+const SignupPage = (props) => {
+  return (
+    <form>
+      <label for="email">Email</label><br />
+      <input type="text" id="email" name="email" /><br />
+      <label for="pwd">Password</label><br />
+      <input type="password" id="pwd" name="pwd" />
+    </form>
+  );
 }
 
 export default App;
